85ae9ad206543dd22878378218f8054aa56901a9
Aris-cotumbeanu <Aris.cotum@gmail.com>
2026-09-21 19:39:47 +0100

    Add files via upload

---
+++ secretbadge.py
@@ -0,0 +1,22 @@
+name = input("enter your real name ,Agent: ")
+gadget = input("enter your favourite gadget: ")
+
+agent_number = 7
+speed_rating = 9.5
+mission_count = 12
+hieght_m = 1.65
+is_active = True
+
+print("name:", name, "-> type", type(name))
+print("gadget:",gadget,"-> type", type (gadget))
+print("agent_number:",agent_number,"-> type", type (agent_number))
+print("speed_rating:",speed_rating,"-> type", type (speed_rating))
+print("mission_count:",mission_count,"-> type", type (mission_count))
+print("hieght (M):",hieght_m,"-> type", type (hieght_m))
+print("is active):",is_active,"-> type", type (is_active))
+
+agent_number_text = str(agent_number)
+mission_count_text = str(mission_count)
+
+print("agent_number:",agent_number,"-> type", type (agent_number))
+print("mission_count:",mission_count,"-> type", type (mission_count))
